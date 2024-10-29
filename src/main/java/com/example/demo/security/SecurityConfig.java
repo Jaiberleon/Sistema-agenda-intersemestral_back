41da96ca8cbf0475167be84ca80f2424600efc0e
jaiberleon <jaleong-2021a@corhuila.edu.co>
2024-10-29 09:40:22 -0500

    incluir swagger, prueba unitaria Admin

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -32,14 +32,19 @@ public class SecurityConfig  {
 public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
             .authorizeHttpRequests(authorize -> authorize
-                    .anyRequest().permitAll() // Permitir todas las solicitudes sin autenticación
+                    // Permitir el acceso a Swagger UI y su documentación
+                    .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui/index.html").permitAll()
+                    // Permitir las rutas de login y registro sin autenticación
+                    .requestMatchers("/api/user/login", "/api/user/register").permitAll()
+                    // Todas las demás solicitudes requieren autenticación
+                    .anyRequest().authenticated()
             )
-            .httpBasic(withDefaults()) // Opcional, si no deseas autenticación básica
+            .httpBasic(withDefaults()) // Si deseas autenticación básica, de lo contrario, puedes eliminar esta línea
             .csrf(csrf -> csrf.disable()); // Deshabilitar CSRF si es necesario
 
     return http.build();
 }
-    @Bean
+@Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
