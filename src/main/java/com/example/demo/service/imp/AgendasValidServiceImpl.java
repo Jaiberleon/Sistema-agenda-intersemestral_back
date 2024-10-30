e776656ab0b28687626ea2fe1aeed8b9c2e075e5
jaiberleon <jaleong-2021a@corhuila.edu.co>
2024-10-30 10:31:14 -0500

    cambio de string quemados por constantes, servicios Agendas service

--- src/main/java/com/example/demo/service/imp/AgendasValidServiceImpl.java
+++ src/main/java/com/example/demo/service/imp/AgendasValidServiceImpl.java
@@ -8,6 +8,7 @@ import com.example.demo.model.UserEntity;
 import com.example.demo.repository.AgendasValidRepository;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.AgendasValidService;
+import com.example.demo.utils.ConstanResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -30,14 +31,14 @@ public class AgendasValidServiceImpl implements AgendasValidService {
     @Override
         public AgendasValid findAgendaById(Long id) {
             return agendasValidRepository.findById(id)
-                    .orElseThrow(() -> new RuntimeException("Agenda no encontrada"));
+                    .orElseThrow(() -> new RuntimeException(ConstanResponses.AGENDA_NOT_FOUND));
         }
 
     @Override
     public ResponseDto<AgendasValid> saveAgenda(MultipartFile file, Long userId, String facultad, String programa) {
         try {
                 UserEntity user = userRepository.findById(userId)
-                    .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+                    .orElseThrow(() -> new RuntimeException(ConstanResponses.USER_NOT_FOUND));
 
             AgendasValid agenda = new AgendasValid();
             agenda.setFileName(file.getOriginalFilename());
@@ -47,41 +48,41 @@ public class AgendasValidServiceImpl implements AgendasValidService {
             agenda.setFile(file.getBytes());
             agenda.setTeacher(user);
             agendasValidRepository.save(agenda);
-            return new ResponseDto<>(null, HttpStatus.CREATED.value(), "Agenda creada correctamente");
+            return new ResponseDto<>(null, HttpStatus.CREATED.value(), ConstanResponses.AGENDA_NOT_FOUND);
         } catch (Exception e){
-            return new ResponseDto<>(null,HttpStatus.BAD_REQUEST.value(), "Ocurrio un error en el guardado de la agenda: "+e.getMessage());
+            return new ResponseDto<>(null,HttpStatus.BAD_REQUEST.value(), ConstanResponses.SERVICE_ERROR +e.getMessage());
     }
     }
 
     @Override
     public ResponseDto<List<IAgendaDto>> findProgramAgendasForDirector(Long programaId) {
         try {UserEntity user = userRepository.findById(programaId)
-                    .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+                    .orElseThrow(() -> new RuntimeException(ConstanResponses.USER_NOT_FOUND));
             String role = userRepository.getRole(programaId);
-            if (role.equals("Director De programa")){
+            if (role.equals(ConstanResponses.DIRECTOR)){
                 List<IAgendaDto> agendas = agendasValidRepository.findAllToDirector(user.getProgram().getId());
-                return new ResponseDto<>(agendas,HttpStatus.OK.value(), "OK");
+                return new ResponseDto<>(agendas,HttpStatus.OK.value(), ConstanResponses.OK);
             }else {
-                return new ResponseDto<>(null,HttpStatus.BAD_REQUEST.value(), "Valida tu rol");
+                return new ResponseDto<>(null,HttpStatus.BAD_REQUEST.value(), ConstanResponses.VALIDATE_ROL);
             }
         } catch (Exception e) {
-            return new ResponseDto<>(null,HttpStatus.BAD_REQUEST.value(), "Error en el servicio");
+            return new ResponseDto<>(null,HttpStatus.BAD_REQUEST.value(), ConstanResponses.SERVICE_ERROR);
         }
     }
 
     @Override
     public ResponseDto<List<IAgendaDto>> findHistoricalProgramAgendasForDirector(Long programaId) {
         try {UserEntity user = userRepository.findById(programaId)
-                .orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+                .orElseThrow(() -> new RuntimeException(ConstanResponses.USER_NOT_FOUND));
             String role = userRepository.getRole(programaId);
-            if (role.equals("Director De programa")){
+            if (role.equals(ConstanResponses.DIRECTOR)){
                 List<IAgendaDto> agendas = agendasValidRepository.findAllForHistoricDirector(user.getProgram().getId());
-                return new ResponseDto<>(agendas,HttpStatus.OK.value(), "OK");
+                return new ResponseDto<>(agendas,HttpStatus.OK.value(), ConstanResponses.OK);
             }else {
-                return new ResponseDto<>(null,HttpStatus.BAD_REQUEST.value(), "Valida tu rol");
+                return new ResponseDto<>(null,HttpStatus.BAD_REQUEST.value(), ConstanResponses.VALIDATE_ROL);
             }
         } catch (Exception e) {
-            return new ResponseDto<>(null,HttpStatus.BAD_REQUEST.value(), "Error en el servicio");
+            return new ResponseDto<>(null,HttpStatus.BAD_REQUEST.value(), ConstanResponses.SERVICE_ERROR);
         }
     }
 
--- src/main/java/com/example/demo/utils/ConstanResponses.java
+++ src/main/java/com/example/demo/utils/ConstanResponses.java
@@ -4,5 +4,13 @@ public class ConstanResponses {
 
     public static final String RESPONSE_CORRECT_GET_USERS_ADMIN  = "User list retrieved successfully";
     public static final String RESPONSE_INCORRECT_GET_USERS_ADMIN  = "User list retrieved successfully";
+    public static final String OK  = "OK";
+    public static final String VALIDATE_ROL  = "Validate your role";
+    public static final String SERVICE_ERROR = "Service error";
+    public static final String DIRECTOR = "Director De programa";
+    public static final String USER_NOT_FOUND = "User not found";
+    public static final String AGENDA_NOT_FOUND = "Agenda not found";
+    public static final String AGENDA_CREATED_CORRECTLY = "Agenda created correctly";
+
 
 }
